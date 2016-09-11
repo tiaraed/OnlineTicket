570eada1b2a0a4c815e3a7889b9670f386ac01f2
Mokleters <tiara.eka99@gmail.com>
2016-09-12 04:08:09 +0700

    Second Commit, melakukan pengkodingan pada radio button, agar hasil dari pilihan radio button bisa diseleksi dan tertulis di halaman page

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5034/formbookingticket/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5034/formbookingticket/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     EditText etNO;
     Button bSub;
     TextView tvHasil;
+    RadioButton rbLA, rbBA, rbWA, rbTA;
 
 
     @Override
@@ -24,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
         etNO = (EditText) findViewById(R.id.editTextNO);
         bSub = (Button) findViewById(R.id.buttonSub);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        rbLA = (RadioButton) findViewById(R.id.radioButtonLA);
+        rbBA = (RadioButton) findViewById(R.id.radioButtonBA);
+        rbWA = (RadioButton) findViewById(R.id.radioButtonWA);
+        rbTA = (RadioButton) findViewById(R.id.radioButtonTA);
+
+
 
         bSub.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +39,22 @@ public class MainActivity extends AppCompatActivity {
 
                 String nama = etNama.getText().toString();
                 int no = Integer.parseInt(etNO.getText().toString());
-                tvHasil.setText("Nama : " + nama + "\n" + "Number : " + no);
+                String hasil = null;
+
+                if (rbLA.isChecked()) {
+                    hasil = rbLA.getText().toString();
+                } else if (rbBA.isChecked()) {
+                    hasil = rbBA.getText().toString();
+                } else if (rbWA.isChecked()) {
+                    hasil = rbWA.getText().toString();
+                } else if (rbTA.isChecked()) {
+                    hasil = rbTA.getText().toString();
+                }
+                if (hasil == null) {
+                    tvHasil.setText("Please Choose Your Plane");
+                } else {
+                    tvHasil.setText("Name : " + nama + "\n" + "Number : " + no + "\n" + "Your Plane : " + hasil);
+                }
 
             }
         });
